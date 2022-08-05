13415e91b9a9e8474ac3168de6a772faf6f302ad
shubham J <shubham.jaiswal@vkaps.com>
2022-08-05 18:43:01 +0530

    checking

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel shubham CI CD
                 </div>
 
                 <div class="links">
